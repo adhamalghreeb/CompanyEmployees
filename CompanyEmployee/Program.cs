086e281a83e9eb1de023b113dbb7d14121dca58e
adhamghreeb <adhamghreeb@gmail.com>
2024-01-07 20:29:06 +0200

    Preparing the projects under union architecture Make Logging system Make Custom exception handling simple get&get all for employee and company

--- CompanyEmployee/Program.cs
+++ CompanyEmployee/Program.cs
@@ -15,14 +15,18 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddAutoMapper(typeof(Program));
 
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
-else
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+if (app.Environment.IsProduction())
     app.UseHsts();
+
 
 // Configure the HTTP request pipeline.
 
--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
+using Entities.Models;
 
 namespace Contracts
 {
     public interface ICompanyRepository
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        Company GetCompany(Guid companyId, bool trackChanges);
+
+
     }
 }
